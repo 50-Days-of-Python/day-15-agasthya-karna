b5e8096b19b0503f0ba3391f1d23c76ea04ba29b
Agasthya Karnamadakala <104967395+agasthya-karna@users.noreply.github.com>
2023-01-28 21:49:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #Write your code here.
+l=[]
+n=int(input())
+for i in range(n):
+    l.append(int(input()))
+        
+l1=[]
+for j in range(len(l)):
+    c=0
+    s=''
+    y=len(str(l[j]))
+    x=str(l[j])[::-1]
+    for i in range(y):
+        if(y%2==0):
+            if(c%3==0 and i!=y-1 and i!=0):
+                s=s+","
+            s=s+x[i]
+            c=c+1
+        else:
+            if(c%3==0 and i!=y and i!=0):
+                s=s+","
+            s=s+x[i]
+            c=c+1
+    l1.append(s[::-1])
+
+print(l1)
